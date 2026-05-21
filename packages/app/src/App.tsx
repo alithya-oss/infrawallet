08e12dd3aad06e9a2bf1ba604fd820e6ea8db981
Florian JUDITH <florian.judith@alithya.com>
2026-05-21 13:07:10 -0400

    fix: restore catalog
    Signed-off-by: Florian JUDITH <florian.judith@alithya.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,6 +1,7 @@
 import { createApp } from '@backstage/frontend-defaults';
+import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
 
 export default createApp({
-  features: [navModule],
+  features: [catalogPlugin, navModule],
 });
